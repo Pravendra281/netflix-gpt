6fa4884a5bda8f5c97f8eca85429b9b649d0f95c
Pravendra <pravendrasingh281@gmail.com>
2023-09-08 20:43:01 +0530

    cra & tailwindcss

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-  <h1 className="font-serif text-4xl text-red-300 bg-slate-50">Namaste</h1>
+  <h1 className="font-serif text-4xl text-red-300 bg-slate-50">Namaste everyone</h1>
     </div>
   );
 }
